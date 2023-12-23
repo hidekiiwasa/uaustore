2af266a51bd594ed95d6b09afdacff8037351585
hidekiiwasa <Spider321011@hotmail.com>
2023-12-23 07:31:12 -0300

    deploy do projeto

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+// import { } from 'react';
+// import { Link } from "react-router-dom";
+// import { CiSearch } from "react-icons/ci";
+
+// function Header() {
+//     return(
+//         <>
+//             <header>
+//                 <nav>
+//                     <div className="superior__nav">
+//                         <div className="logo__header">
+//                             <Link>
+//                                 {/* <img src={} alt="" /> */}
+//                                 uaustore
+//                             </Link>
+//                         </div>
+//                         <div className="barra-pesquisar__header">
+//                             <form action="">
+//                                 <input type="search" />
+//                                 <button>
+//                                     <CiSearch />
+//                                 </button>
+//                             </form>
+//                         </div>
+//                         <div className="menu__nav">
+//                             <ul>
+//                                 <li><Link>Cadastrar</Link></li>
+//                                 <li><Link>Entrar</Link></li>
+//                             </ul>
+//                         </div>
+//                     </div>
+//                     <div className="inferior__nav">
+                        
+//                     </div>
+//                 </nav>
+//             </header>
+//         </>
+//     )
+// }
