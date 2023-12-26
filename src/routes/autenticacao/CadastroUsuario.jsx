6b19915b015b11dcca1a4822875b3a5005000110
hidekiiwasa <Spider321011@hotmail.com>
2023-12-26 15:23:50 -0300

    .

--- src/routes/autenticacao/CadastroUsuario.jsx
+++ src/routes/autenticacao/CadastroUsuario.jsx
@@ -1,26 +1,14 @@
 import { } from 'react';
+import Card from '../../components/CardAutenticacao'
 
 function CadastroUsuario() {
 
+
+    
     return(
         <>
-            <div className="secaoCadastroUsuario">
-                <div className="cardLogin">
-                    <form onSubmit={handleSubmit}>
-                        <legend>LOGIN</legend>
-                        <div className="inputs">
-                            <div className="inputContainer">
-                                <label htmlFor="usuario"><FaUser /></label>
-                                <input type="text" value={login.nomeDeUsuario} name="nomeDeUsuario" placeholder="Digite o seu usuário" onChange={handleChange} required/>
-                            </div>
-                            <div className="inputContainer">
-                                <label htmlFor="password"><MdOutlinePassword /></label>
-                                <input type="password" value={login.senha} name="senha" placeholder="Digite a sua senha" onChange={handleChange} required/>
-                            </div>
-                        </div>
-                        <button type="submit">LOGAR</button>
-                    </form>
-                </div>
+            <div className="secaoLogin">
+                <Card cardAuthLogin = {false} />
             </div>
         </>
     )
