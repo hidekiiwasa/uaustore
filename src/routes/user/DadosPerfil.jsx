2ddaeef1c95b601ae3b19ab6f68d8cf8b3a500dc
hidekiiwasa <Spider321011@hotmail.com>
2024-03-13 06:21:29 -0300

    Melhoria na lógica

--- src/routes/user/DadosPerfil.jsx
+++ src/routes/user/DadosPerfil.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useState, useContext } from 'react';
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from '../../contexts/ContextoAuth';
 
 function DadosPerfil() {
 
-    const { usuarioData, token } = useContext(AuthContext);
+    // A API está enviando a senha e o nomeDeUsuario incorretos por algum motivo, logo não é possível mudar o usuário. Porém, creio que esteja funcionando direito a lógica.
+
+    const { usuarioData, token, setToken } = useContext(AuthContext);
 
     const [dados, setDados] = useState({
         apelido: '',
@@ -28,38 +31,41 @@ function DadosPerfil() {
         };
 
         dadosUserInfo();
-    }, []);
+    }, [token]);
 
-    const handleChangeLogin = async (e) => {
+    const handleChangeAlterar = async (e) => {
         const { name, value } = e.target;
-        setLogin({ ...login, [name]: value });
+        setDados({ ...dados, [name]: value });
     };
 
-    const updateUser = async (token) => {
-    
-        const userData = {
-          apelido: "bbqw4",
-          nomeDeUsuario: "111111",
-          senha: "111111",
-          email: "564qwwa@hotmail.com",
-          telefone: "123321"
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await updateUser(dados, token);
+        } catch (error) {
+            console.error('Erro:', error.message);
         }
+    };
+
+    const updateUser = async (dataUser, token) => {
+        
     
         try {
-    
+            console.log(dataUser)
           const response = await fetch('https://testeuaustorafrotaf.onrender.com/usuario/atualizar', {
             method: 'PUT',
-            body: JSON.stringify(userData),
+            body: JSON.stringify(dataUser),
             headers: {
               Authorization: `Bearer ${token}`,
               'Content-Type': 'application/json; charset=UTF-8',
             },
           });
-    
-          if (response.ok) {
-            console.log('Usuário atualizado com sucesso!');
 
-          } else {
+          const data = await response.json()
+          const novoToken = data.token
+          setToken(novoToken)
+
+          if (!response.ok) {
             console.error('Falha ao atualizar usuário');
 
           }
@@ -77,10 +83,9 @@ function DadosPerfil() {
                     <div className="box__dado-perfil">
                         <span>Email</span>
                         <div className="hidden">
-                            <p>{dados.email}</p>
-                            <form>
-                                <input type="email" />
-                                <button>Enviar</button>
+                            <form onSubmit={handleSubmit}>
+                                <input type="email" value={dados.email} name="email" onChange={handleChangeAlterar}/>
+                                <button type='submit'>Enviar</button>
                             </form>
                         </div>
                         <button>Alterar</button>
@@ -88,10 +93,9 @@ function DadosPerfil() {
                     <div className="box__dado-perfil">
                         <span>Telefone</span>
                         <div className="hidden">
-                            <p>{dados.telefone}</p>
-                            <form>
-                                <input type="tel" />
-                                <button>Enviar</button>
+                            <form onSubmit={handleSubmit}>
+                                <input type="tel" value={dados.telefone} name="telefone" onChange={handleChangeAlterar}/>
+                                <button type='submit'>Enviar</button>
                             </form>
                         </div>
                         <button>Alterar</button>
@@ -99,10 +103,9 @@ function DadosPerfil() {
                     <div className="box__dado-perfil">
                         <span>Nome de usuário</span>
                         <div className="hidden">
-                            <p>{dados.nomeDeUsuario}</p>
-                            <form>
-                                <input type="text" />
-                                <button>Enviar</button>
+                            <form onSubmit={handleSubmit}>
+                                <input type="text" value={dados.nomeDeUsuario} name="nomeDeUsuario" onChange={handleChangeAlterar}/>
+                                <button type='submit'>Enviar</button>
                             </form>
                         </div>
                         <button>Alterar</button>
@@ -110,10 +113,9 @@ function DadosPerfil() {
                     <div className="box__dado-perfil">
                         <span>Apelido</span>
                         <div className="hidden">
-                            <p>{dados.apelido}</p>
-                            <form>
-                                <input type="text" />
-                                <button>Enviar</button>
+                            <form onSubmit={handleSubmit}>
+                                <input type="text" value={dados.apelido} name="apelido" onChange={handleChangeAlterar}/>
+                                <button type='submit'>Enviar</button>
                             </form>
                         </div>
                         <button>Alterar</button>
@@ -121,13 +123,16 @@ function DadosPerfil() {
                     <div className="box__dado-perfil">
                         <span>Senha</span>
                         <div className="hidden">
-                            <p>*******</p>
-                            <form>
-                                <input type="password" />
-                                <button>Enviar</button>
+                            <form onSubmit={handleSubmit}>
+                                <p>{dados.senha} bb</p>
+                                <input type="password" value={dados.senha} name="senha" onChange={handleChangeAlterar} placeholder='*******'/>
+                                <button type='submit'>Enviar</button>
                             </form>
                         </div>
                         <button>Alterar</button>
+                    </div>
+                    <div className="box__dado-perfil">
+                        <Link to='/excluirperfil'>Excluir conta</Link>
                     </div>
                 </div>
             </div>
