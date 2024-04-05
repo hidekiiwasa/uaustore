9e01243c47a2d3d6eb4d4e127db5b3981117eae9
hidekiiwasa <Spider321011@hotmail.com>
2024-04-05 14:29:19 -0300

    removendo usenavigate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext } from 'react';
 import { AuthContext } from '../contexts/ContextoAuth';
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { CiSearch } from "react-icons/ci";
 import { CgProfile } from "react-icons/cg";
 
@@ -16,7 +16,7 @@ function Header() {
                 const userInfo = await usuarioData(token);
                 setApelido(userInfo.apelido); 
             } 
-        };
+        }
 
         headerUserInfo();
     }, [logado]);
